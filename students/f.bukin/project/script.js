7388657936474f97661a51c5df681eef2a33ad55
Philip <Philipbukin@gmail.com>
2020-05-12 01:25:02 +0300

    Lesson 2 Hometask

--- students/f.bukin/project/script.js
+++ students/f.bukin/project/script.js
@@ -1,18 +1,67 @@
-const goods = [
-    { title: 'Shirt', price: 100 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 200 },
-    { title: 'Shoes', price: 300 },
-    {}
-];
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
 
-const getGoodsItem = (title = "Товар не найден", price = 0) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-};
-
-const renderGoodsList = list => {
-    const goodsList = list.map(item => getGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList;
+    render() {
+        return `<div class="goods-item"><h3>${this.title}</h3><p>${this.price}</p></div>`;
+    }
 }
 
-renderGoodsList(goods);
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 100 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 200 },
+            { title: 'Shoes', price: 300 },
+        ];
+    }
+
+    render() {
+        let goodsList = '';
+        this.goods.forEach(({ title, price }) => {
+            const goodItem = new GoodsItem(title, price);
+            goodsList += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = goodsList;
+    }
+
+    sumPrice(){
+        let sumAllGoods = 0;
+        this.goods.forEach(({price}) => {
+            sumAllGoods += price;
+        });
+        return sumAllGoods;
+    }
+}
+class Basket extends GoodsList{
+    countGoods () {
+        let countGoods = 0;
+        this.goods.forEach(() => {
+            countGoods++;
+        });
+        document.querySelector('.cart-button').insertAdjacentHTML("beforeend",  ' (' + countGoods + ')');
+    }
+    render() {
+        let goodsList = '';
+        this.goods.forEach(({ title, price }) => {
+            const goodItem = new GoodsItem(title, price);
+            goodsList += goodItem.render();
+        });
+        document.querySelector('.basket-goods-list').innerHTML = goodsList;
+        document.querySelector('.basket-goods-list').insertAdjacentHTML("beforeend",  `<div class="sum-goods"><h3>Итого: ${this.sumPrice()}</h3></div>`)
+    }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+let basket = new Basket();
+basket.fetchGoods();
+basket.countGoods();
